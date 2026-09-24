8c205a70c926e06307606705d05ace3fe02ac2f9
gustavozgoda-ctrl <gustavo.zgoda@escola.pr.gov.br>
2026-09-24 08:33:23 -0300

    Create perguntas.js

---
+++ js/perguntas.js
@@ -0,0 +1,41 @@
+const perguntas = [
+    {
+        enunciado: `Você é ${nomeHeroi}, um corajoso aventureiro que adentrou o Labirinto de Aethelgard em busca de ${reliquia}. Na entrada, você encontra dois caminhos iluminados por tochas misteriosas: um corredor estreito à esquerda com eco de água fluindo, e um portão de ferro à direita marcado com runas brilhantes.`,
+        alternativas: [
+            {
+                texto: "Seguir pelo corredor estreito com som de água.",
+                afirmacao: "Você escolheu o caminho mais silencioso e úmido, "
+            },
+            {
+                texto: "Atravessar o portão de ferro com runas.",
+                afirmacao: "Você preferiu encarar o mistério direto das runas mágicas, "
+            }
+        ]
+    },
+    {
+        enunciado: "Avançando no caminho escolhido, você encontra um altar antigo com um baú de madeira podre guardado por um Goblin adormecido. Ao lado, há uma passagem secreta semiaberta.",
+        alternativas: [
+            {
+                texto: "Tentar passar de fininho pelo Goblin para pegar o baú.",
+                afirmacao: "demonstrou ousadia ao tentar pilhar o baú em silêncio "
+            },
+            {
+                texto: "Ignorar o Goblin e entrar direto pela passagem secreta.",
+                afirmacao: "agiu com prudência ao evitar um combate desnecessário "
+            }
+        ]
+    },
+    {
+        enunciado: "Mais a fundo nas catacumbas, uma porta mágica bloqueia sua jornada final. Um espírito guardião surge e faz uma pergunta: 'O que é mais forte que o aço, mas quebra com uma única palavra livre?'",
+        alternativas: [
+            {
+                texto: "Responder 'O Silêncio'.",
+                afirmacao: "e provou ter grande sabedoria para resolver enigmas ancestrais."
+            },
+            {
+                texto: "Responder 'O Destino'.",
+                afirmacao: "e confiou na força das palavras nobres para abrir caminho."
+            }
+        ]
+    }
+];
